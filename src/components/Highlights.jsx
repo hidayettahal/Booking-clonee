0549421aa3c2d941fe04394eb4954910b2afd2ce
hidayettahal <tahal.hidayet79097@gmail.com>
2025-09-07 16:16:42 +0300

    Highligtsın hataları ve eksikleri düzeltildi

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -7,6 +7,35 @@ function Highlights() {
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
 
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const cities = [
+    {
+      id: 1,
+      name: "Monaco",
+      reasons: ["historical place", "casino", "nightlife"],
+      imageClass: "show-4",
+    },
+    {
+      id: 2,
+      name: "San Antonio Bay",
+      reasons: ["sunsets", "beaches", "nightlife"],
+      imageClass: "show-2",
+    },
+    {
+      id: 3,
+      name: "Ibiza Town",
+      reasons: ["sunsets", "beaches", "nightlife"],
+      imageClass: "show-3",
+    },
+    {
+      id: 4,
+      name: "Paris",
+      reasons: ["museums", "sightseeing", "culture"],
+      imageClass: "show",
+    },
+  ];
+
   const handleMouseDown = (e) => {
     setIsDown(true);
     setStartX(e.pageX - sliderRef.current.offsetLeft);
@@ -24,150 +53,63 @@ function Highlights() {
     sliderRef.current.scrollLeft = scrollLeft - walk;
   };
 
+  const handleSelectCity = (city, index) => {
+    setSelectedCity(city);
+
+    const item = sliderRef.current.children[index * 2];
+    if (item) {
+      item.scrollIntoView({ behavior: "smooth", inline: "start" });
+    }
+  };
+
   return (
     <section className="Highlights">
-      <div className="Container">
-        <div
-          className="Highlights-slider"
-          ref={sliderRef}
-          onMouseDown={handleMouseDown}
-          onMouseLeave={handleMouseLeave}
-          onMouseUp={handleMouseUp}
-          onMouseMove={handleMouseMove}
-        >
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
+      <div
+        className="Highlights-slider"
+        ref={sliderRef}
+        onMouseDown={handleMouseDown}
+        onMouseLeave={handleMouseLeave}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        {cities.map((city, index) => (
+          <React.Fragment key={city.id}>
+            <div
+              className={`Highlights-item ${
+                selectedCity?.id === city.id ? "active" : ""
+              }`}
+              onClick={() => handleSelectCity(city, index)}
+            >
+              <div className={city.imageClass}>
+                <div className="content">
+                  <h4 className="title">{city.name}</h4>
+                  <div className="tag">
+                    <h5 className="tag-title">Top reasons to visit:</h5>
+                    <div className="tag-list">
+                      {city.reasons.map((reason, i) => (
+                        <span key={i} className="tag-item">
+                          {reason}
+                          {i < city.reasons.length - 1 ? ", " : ""}
+                        </span>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
-                  </div>
-                </div>
+
+            {/* Eğer seçilen şehir buysa hemen yanına açıklama kutusu koy */}
+            {selectedCity?.id === city.id && (
+              <div className="Highlights-details">
+                <h3>Still planning a trip to {selectedCity.name}?</h3>
+                <p>
+                  <b>23 Oct - 24 Oct</b> / 1 room, 2 adults
+                </p>
+                <button className="Highlights-btn">View properties</button>
               </div>
-            </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-          <div className="Highlights-item">
-            <div className="show">
-              <div className="content">
-                <h4 className="title">Paris</h4>
-                <div className="tag">
-                  <h5 className="tag-title">Top reasons to visit:</h5>
-                  <div className="tag-list">
-                    <a href="" className="tag-list">
-                      museums
-                    </a>
-                    <a href="" className="tag-list">
-                      sigtseeing,
-                    </a>
-                    <a href="" className="tag-list">
-                      culture
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="Highlights-item-extra"></div>
-          </div>
-        </div>
+            )}
+          </React.Fragment>
+        ))}
       </div>
     </section>
   );
