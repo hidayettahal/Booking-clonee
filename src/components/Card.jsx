32eccb8dedc236d2408f54802d46d66a47f6270c
hidayettahal <tahal.hidayet79097@gmail.com>
2025-09-13 23:56:26 +0300

    cards tamamlandı

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import { ImLifebuoy } from "react-icons/im";
-
+import reviewsIcon from "../assets/images/reviews.svg";
 import "./Card.css";
 function Card() {
   return (
-    <section>
-      <div className="container-cards">
-        <div className="cards">
-          <div className="card-logo"></div>
-          <div className="card-content">
-            <h5 className="item-title">
-              <span>131,130,000</span>
-              reviews
-            </h5>
-            <p className="item-text">
-              Real guest. Real stays. Real option to help you make the right
-              choice
-            </p>
-          </div>
+    <div className="container-cards">
+      <div className="cards">
+        <div className="cards-logo">
+          <img src={reviewsIcon} alt="Reviews Icon" />
         </div>
-
-        <div className="cards">
-          <div className="cards-logo">
-            <p>?</p>
-            <ImLifebuoy />
-          </div>
+        <div className="cards-content">
+          <h5 className="item-title">
+            <span>131,130,000 </span>
+            reviews
+          </h5>
+          <p className="item-text">
+            Real guest. Real stays. Real option to help you make the right
+            choice
+          </p>
         </div>
       </div>
-    </section>
+
+      <div className="cards">
+        <div className="cards-logo">
+          <p>?</p>
+          <ImLifebuoy className="icon-help" />
+        </div>
+        <div className="cards-content">
+          <h5 className="item-title">
+            We 're here to help
+            <span> 24/7</span>
+          </h5>
+          <p className="item-text">
+            Find answers, manage your booking and more in our
+            <a href="#">Help Center</a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
