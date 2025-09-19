581ded0aaf3554d0558e1f90d8cf8f3e4ac88935
hidayettahal <tahal.hidayet79097@gmail.com>
2025-09-19 20:06:42 +0300

    reviews tamamlandı

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import "./Reviews.css";
 function Reviews() {
   return (
-    <section>
-      <div className="container header">
+    <section className="Reviews">
+      <div className="container-header-rev">
         <h1 className="index-title">Still deciding where to stay?</h1>
         <h3 className="subtitle-reviews">
           We want to make your decision easier! With over 131,150,000 verified
           guest reviews, we can help you find the perfect place to stay.
         </h3>
       </div>
-      <div className="container button-large">
+      <div className="container-button-large">
         <div className="btn-active">Hotel reviews</div>
         <div className="btn-rew">B&B & inn reviews</div>
         <div className="btn-rew">Holiday rental reviews</div>
       </div>
-      <div className="container Rev-item">
+      <div className="container-Rev-item">
         <div className="Reviews-item">
           <h4 className="Reviews-item-title">Edinburgh</h4>
           <div className="Reviews-item-subtitle">
@@ -59,7 +59,7 @@ function Reviews() {
           </div>
         </div>
         <div className="Reviews-item">
-          <h4 className="Reviews-item-title"></h4>
+          <h4 className="Reviews-item-title">Düsseldorf</h4>
           <div className="Reviews-item-subtitle">
             <small>283,435 hotel reviews</small>
           </div>
